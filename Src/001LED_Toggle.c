9aa6313281b068fbf42fba33d5c0049df89a5a16
Dhanush D <dhanushd@Dhanushs-MacBook-Air.local>
2026-01-15 22:50:33 +0530

    adding a file to ckeck if the driver definitions are correct. This code blinks the LD2 on the board with some small delay(Software delay using for loop)

---
+++ Src/001LED_Toggle.c
@@ -0,0 +1,39 @@
+/*
+ * 001LED_Toggle.c
+ *
+ *  Created on: 15 Jan 2026
+ *      Author: dhanushd
+ */
+
+// To blink the user LED LD2 which is connected to PA5 pin 
+#include<stdint.h>
+#include "stm32G491xx.h"
+
+void delay(void){
+    for(uint32_t i = 0; i < 500000; i++);
+}
+
+int main(void){
+
+    GPIO_Handle_t Gpioled;
+    // Select the port for pin PA5
+    Gpioled.pGPIOx = GPIOA;
+    // Pin configurations				
+    Gpioled.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_5;
+    Gpioled.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+    Gpioled.GPIO_PinConfig.GPIO_PinSpeed = GPIO_OUT_SPEED_HIGH;
+    Gpioled.GPIO_PinConfig.GPIO_PinOType = GPIO_MODE_OUT_PUPL;
+    Gpioled.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_NO_PUPD;
+
+    // Enable the clock for the GPIOA peripheral
+    GPIO_PCLK_CTRL(GPIOA, ENABLE);
+    // Initialise the GPIOA register with the above declared variables
+    GPIO_Init(&Gpioled);
+
+    // Infinite loop
+    while(1){
+        GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_5);
+        delay(); 
+    }
+    return 0;
+}
